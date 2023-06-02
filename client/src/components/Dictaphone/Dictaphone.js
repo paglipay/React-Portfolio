0154ab563656e036702f7de3fdabcec89c6988b1
Paul <paglipay@gmail.com>
2023-06-01 20:46:03 -0700

    removed end

--- client/src/components/Dictaphone/Dictaphone.js
+++ client/src/components/Dictaphone/Dictaphone.js
@@ -126,7 +126,6 @@ const Dictaphone = () => {
                         {
                           True: `${prompt}`,
                         },
-                        "end",
                       ],
                     },
                   ],
