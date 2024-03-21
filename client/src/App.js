e92a351a2950b25497df84dc4c0bf51a8d8dbaf2
Paul <paglipay@gmail.com>
2024-03-21 11:18:38 -0700

    papercups storytime

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Login from "./components/Login";
@@ -9,47 +9,62 @@ import NoMatch from "./components/pages/NoMatch";
 import { VideoChatProvider } from "./utils/GlobalState";
 // import logo from './logo.svg';
 // import './App.css';
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import axios from 'axios';
-import Todos from './components/Todos/components/TodoList';
-import EmployeeDirectory from './components/EmployeeDirectory/components/Main'
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import axios from "axios";
+import Todos from "./components/Todos/components/TodoList";
+import EmployeeDirectory from "./components/EmployeeDirectory/components/Main";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Devices from "./components/Devices";
 import PrivateRoute from "./components/PrivateRoute";
 import TouchlessLogin from "./components/TouchlessLogin/TouchlessLoginUrl";
-import EmployeeAppointments from './components/EmployeeAppointments/AppointmentsContainer';
-import DTree from './components/DTree';
+import EmployeeAppointments from "./components/EmployeeAppointments/AppointmentsContainer";
+import DTree from "./components/DTree";
 // import DynamicForm from './components/DTree/DynamicForm';
-import AiChat from './components/AiChat/AiChat'
-import Navbar from './components/SideNavBar/Navbar';
-import ReactChatWindow from './components/ReactChatWindow/ReactChatWindow';
+import AiChat from "./components/AiChat/AiChat";
+import Navbar from "./components/SideNavBar/Navbar";
+// import ReactChatWindow from "./components/ReactChatWindow/ReactChatWindow";
+import { ChatWidget } from "@papercups-io/chat-widget";
+import { Storytime } from "@papercups-io/storytime";
 
+const st = Storytime.init({
+  accountId: "5453b892-c703-4b9c-be85-43b1f28d47ce",
+
+  // Optionally pass in metadata to identify the customer
+  // customer: {
+  //  name: 'Test User',
+  //  email: 'test@test.com',
+  //  external_id: '123',
+  // },
+
+  // Optionally specify the base URL
+  baseUrl: "https://papercups.paglipay.info",
+});
 function App() {
-
   const [authenticated, setAuthenticated] = useState(false);
 
   const authenticate = () => {
-    setAuthenticated(true)
-  }
+    setAuthenticated(true);
+  };
 
   const deAuthenticate = () => {
-    setAuthenticated(false)
-  }
+    setAuthenticated(false);
+  };
 
   const logout = () => {
-    axios.get('/api/users/logout/1')
+    axios
+      .get("/api/users/logout/1")
       .then(function (data) {
         deAuthenticate();
         window.location.reload();
-      }).catch(function (err) {
+      })
+      .catch(function (err) {
         console.log(err);
       });
-  }
+  };
 
   return (
-
     <Provider store={store}>
       <Router>
         <div>
@@ -59,39 +74,88 @@ function App() {
           <Switch>
             <Route exact path={["/", "/home"]}>
               <Home />
-              <ReactChatWindow />
+              {/* <ReactChatWindow /> */}
+              <ChatWidget
+                // `accountId` is used instead of `token` in older versions
+                // of the @papercups-io/chat-widget package (before v1.2.x).
+                // You can delete this line if you are on the latest version.
+                // accountId="deb5485b-6637-4cb9-8ec1-29e8f4b9e083"
+                token="deb5485b-6637-4cb9-8ec1-29e8f4b9e083"
+                inbox="fd028522-e20d-42c9-816a-b47d835c63cf"
+                title="Welcome to Paglipay"
+                subtitle="Ask us anything in the chat window below 😊"
+                primaryColor="#1890ff"
+                newMessagePlaceholder="Start typing..."
+                showAgentAvailability={false}
+                agentAvailableText="We're online right now!"
+                agentUnavailableText="We're away at the moment."
+                requireEmailUpfront={false}
+                iconVariant="outlined"
+                baseUrl="https://papercups.paglipay.info"
+                // Optionally include data about your customer here to identify them
+                // customer={{
+                //   name: __CUSTOMER__.name,
+                //   email: __CUSTOMER__.email,
+                //   external_id: __CUSTOMER__.id,
+                //   metadata: {
+                //     plan: "premium"
+                //   }
+                // }}
+              />
             </Route>
             <Route exact path={["/", "/contact"]}>
               <Contact />
-              <ReactChatWindow />
+              {/* <ReactChatWindow /> */}
             </Route>
-            <Route exact path="/touchlesslogin/:id" component={TouchlessLogin} />
+            <Route
+              exact
+              path="/touchlesslogin/:id"
+              component={TouchlessLogin}
+            />
 
-            <Route exact path={["/devices"]} authenticated={authenticated} component={Devices} />
+            <Route
+              exact
+              path={["/devices"]}
+              authenticated={authenticated}
+              component={Devices}
+            />
 
             <Route exact path={["/dtree"]} component={DTree} />
 
-            <PrivateRoute exact path={["/devices"]} authenticated={authenticated} component={Devices} />
-            <Route exact path="/login" render={props =>
-              <Login
-                {...props}
-                authenticate={authenticate}
-                deAuthenticate={deAuthenticate}
-                authenticated={authenticated}
-                logout={logout}
-              />}
+            <PrivateRoute
+              exact
+              path={["/devices"]}
+              authenticated={authenticated}
+              component={Devices}
             />
-            <Route exact path="/signup" render={props =>
-              <Signup
-                {...props}
-                authenticate={authenticate}
-                deAuthenticate={deAuthenticate}
-                authenticated={authenticated}
-                logout={logout}
-              />}
+            <Route
+              exact
+              path="/login"
+              render={(props) => (
+                <Login
+                  {...props}
+                  authenticate={authenticate}
+                  deAuthenticate={deAuthenticate}
+                  authenticated={authenticated}
+                  logout={logout}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/signup"
+              render={(props) => (
+                <Signup
+                  {...props}
+                  authenticate={authenticate}
+                  deAuthenticate={deAuthenticate}
+                  authenticated={authenticated}
+                  logout={logout}
+                />
+              )}
             />
             <Route exact path="/lobbylogin">
-              <VideoChatProvider >
+              <VideoChatProvider>
                 <LobbyLogin />
               </VideoChatProvider>
             </Route>
@@ -111,7 +175,6 @@ function App() {
         </div>
       </Router>
     </Provider>
-
   );
 }
 
