aa876c7034608a5297bd16cd2c6f106067c8641d
Paul <paglipay@gmail.com>
2025-04-09 21:32:45 -0700

    9 to 3

--- client/src/components/CameraCapture/CameraCapture.js
+++ client/src/components/CameraCapture/CameraCapture.js
@@ -1,14 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import {
-  Form,
-  Table,
-  Badge,
-  Container,
-  Row,
-  Col,
-  Button,
-  Modal,
-} from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import "./CameraCapture.css";
 
 const CameraBooth = () => {
@@ -61,34 +52,34 @@ const CameraBooth = () => {
 
   return (
     <Row className="camera-booth-container">
-      <div style={{ display: "flex", gap: "20px", padding: "20px" }}>
-        {/* Left Column */}
-        <Col
+      {/* Left Column */}
+      <Col xs={12} md={3} className="camera-booth-left">
+        <div
           style={{
             display: "flex",
             flexDirection: "column",
             alignItems: "flex-end", // Align items to the right
+            gap: "10px",
           }}
-          xs={4}
-          md={4}
-          lg={4}
         >
           {showLiveInLeft ? (
             <div
               style={{
                 position: "relative",
-                width: 320,
-                height: 240,
+                width: "100%",
+                aspectRatio: "4 / 3",
                 border: "2px solid gray",
               }}
             >
               <video
                 ref={videoRef}
-                width="320"
-                height="240"
                 autoPlay
                 muted
-                style={{ objectFit: "cover" }}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
               />
             </div>
           ) : (
@@ -96,57 +87,56 @@ const CameraBooth = () => {
               <img
                 key={idx}
                 src={img}
-                width="320"
-                height="240"
                 alt={`snap-${idx}`}
-                style={{ border: "2px solid #ccc" }}
+                style={{
+                  width: "100%",
+                  aspectRatio: "4 / 3",
+                  border: "2px solid #ccc",
+                }}
               />
             ))
           )}
-        </Col>
-        <Col
-          style={{ display: "flex", flexDirection: "column" }}
-          xs={8}
-          md={8}
-          lg={8}
+        </div>
+      </Col>
+
+      {/* Right Column */}
+      <Col xs={12} md={9} className="camera-booth-right">
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            aspectRatio: "16 / 9",
+            border: "2px solid gray",
+          }}
         >
-          {/* Right Column */}
-
           {!showLiveInLeft && (
-            <div
+            <video
+              ref={videoRef}
+              autoPlay
+              muted
               style={{
-                position: "relative",
-                width: 960,
-                height: 720,
-                border: "2px solid gray",
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
               }}
-            >
-              <video
-                ref={videoRef}
-                width="960"
-                height="720"
-                autoPlay
-                muted
-                style={{ objectFit: "cover" }}
-              />
-            </div>
+            />
           )}
-          <Button
-            onClick={handleCapture}
-            style={{ marginTop: "20px", padding: "10px 20px" }}
-          >
-            Snap Photo
-          </Button>
+        </div>
+        <Button
+          onClick={handleCapture}
+          style={{ marginTop: "20px", padding: "10px 20px" }}
+        >
+          Snap Photo
+        </Button>
 
-          {/* Hidden canvas for image capture */}
-          <canvas
-            ref={canvasRef}
-            width="320"
-            height="240"
-            style={{ display: "none" }}
-          />
-        </Col>
-      </div>
+        {/* Hidden canvas for image capture */}
+        <canvas
+          ref={canvasRef}
+          width="320"
+          height="240"
+          style={{ display: "none" }}
+        />
+      </Col>
     </Row>
   );
 };
